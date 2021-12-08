ff540e2cabf90e9a4ce9d4595d37879eb4f0a177
Mia Song <mia721015@gmail.com>
2021-12-08 16:22:07 +0800

    jwt token

--- admin/view.py
+++ admin/view.py
@@ -1,5 +1,4 @@
 from flask import render_template
-from flask_jwt_extended import jwt_required
 
 from . import admin
 
@@ -7,7 +6,6 @@ from . import admin
 def index():
     return render_template("admin.html")
 
-@jwt_required()
 @admin.route("/board")
 def board():
     return render_template("board.html")
