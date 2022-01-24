2b3d2766ff7b872f853e99e8d107c91eefb945f0
Mia Song <mia721015@gmail.com>
2022-01-25 00:28:58 +0800

    flask mail

--- App/api_v2/email.py
+++ App/api_v2/email.py
@@ -9,7 +9,6 @@ def send_async_email(app, msg):
         mail.send(msg)
 
 def send_email(order_query):
-    print(order_query.booker_email)
     msg = Message(
         subject = f"打個蛋海旅來信：訂房編號{order_query.oid}內容及匯款通知", 
         recipients = [order_query.booker_email, ],
