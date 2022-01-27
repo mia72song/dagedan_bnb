2d35b34fcb7f12dbda67c25e875a9fe44b6d53c3
Mia Song <mia721015@gmail.com>
2022-01-27 21:49:26 +0800

    modify models

--- App/models/model.py
+++ App/models/model.py
@@ -1,18 +1,20 @@
 from sqlalchemy import text
-from datetime import datetime
+from datetime import datetime, date
 from werkzeug.security import generate_password_hash, check_password_hash
 import enum
 
 from App import db
 from .mydb import Mydb
+from App.models import mydb
 
 class Calendar(db.Model):
     date = db.Column(db.Date, primary_key=True)
-    booked = db.relationship("Booking", backref="c", lazy="dynamic")
     day = db.Column(db.String(3), comment="星期")
     is_holiday = db.Column(db.Boolean, default=False, server_default=text('0'))
     note = db.Column(db.String(128))
     is_closed = db.Column(db.Boolean, default=False, server_default=text('0'))
+
+    booked = db.relationship("Booking", backref="c", lazy="dynamic")
 
 class User(db.Model):
     __tablename__ = "users"
@@ -30,7 +32,6 @@ class User(db.Model):
         return check_password_hash(self.password, input_value)
 
 class Booking(db.Model):
-    #  設置外鍵ForeignKey，一對多的『多』，ForeignKey("table_name.col_name")
     date = db.Column(db.Date, db.ForeignKey("calendar.date"), primary_key=True)
     room_no = db.Column(db.String(64), db.ForeignKey("rooms.room_no"), primary_key=True)
     order_id = db.Column(db.String(64), db.ForeignKey("orders.oid"), nullable=False)
@@ -38,8 +39,6 @@ class Booking(db.Model):
 class Order(db.Model):
     __tablename__ = "orders"
     oid = db.Column(db.String(64), primary_key=True)
-    #  設置關聯relationship：一對多的『一』，relationship("類名")
-    booked = db.relationship("Booking", backref="o", lazy="dynamic")
     create_datetime = db.Column(db.DateTime, default=datetime.now)
     check_in_date = db.Column(db.Date, nullable=False)
     check_out_date = db.Column(db.Date, nullable=False)
@@ -58,6 +57,8 @@ class Order(db.Model):
     payment_deadline = db.Column(db.Date, nullable=False)
     payment_id = db.Column(db.String(32), db.ForeignKey("payment_atm.pid"))
 
+    booked = db.relationship("Booking", backref="o", lazy="dynamic")
+
     class OrderStatus(enum.Enum):
         NEW = "NEW"
         PENDING = "PENDING"
@@ -68,7 +69,7 @@ class Order(db.Model):
     status = db.Column(db.Enum(OrderStatus), server_default="NEW")
     update_datetime = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     update_user = db.Column(db.String(64))
-
+        
     def getDataDict(self):
         table_name = self.__tablename__
         pk_col = "oid"
@@ -80,9 +81,10 @@ class Order(db.Model):
 class Room(db.Model):
     __tablename__ = "rooms"
     room_no = db.Column(db.String(64), primary_key=True)
-    booked = db.relationship("Booking", backref="r", lazy="dynamic")
     room_type = db.Column(db.String(64), db.ForeignKey("room_types.type"), nullable=False)
     is_available = db.Column(db.Boolean, default=False, server_default=text("1"))
+
+    booked = db.relationship("Booking", backref="r", lazy="dynamic")
 
 class RoomType(db.Model):
     __tablename__ = "room_types"
@@ -95,6 +97,7 @@ class RoomType(db.Model):
     description = db.Column(db.Text)
     images = db.Column(db.Text)
     is_del = db.Column(db.Boolean, default=False, server_default=text("0"))
+
     rooms = db.relationship("Room", backref="rt", lazy="dynamic")
 
     def getDataDict(self):
@@ -106,8 +109,7 @@ class RoomType(db.Model):
             data_dict = mydb.getAllByPk(table_name, (pk_col, pk_value))
             return data_dict
         else:
-            return None
-        
+            return None    
 
 class PaymentAtm(db.Model):
     __tablename__ = "payment_atm"
@@ -119,6 +121,7 @@ class PaymentAtm(db.Model):
     transfer_date = db.Column(db.Date, nullable=False)
     update_datetime = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     update_user = db.Column(db.String(64))
+
     orders = db.relationship("Order", backref="p", lazy="dynamic")
 
     def getDataDict(self):
