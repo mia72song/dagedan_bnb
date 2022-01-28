28442608b03f2d7424907434052c0cebcd87282d
Mia Song <mia721015@gmail.com>
2022-01-28 18:49:58 +0800

    remove manager and update requirements.txt

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 # -*- coding: utf-8 -*-
 
-from flask_script import Manager
-from flask_migrate import MigrateCommand
-
 from App import create_app
 
 app = create_app()
 
-manager = Manager(app)
-manager.add_command("db", MigrateCommand)
-
-'''依賴降级：
-pip install Flask-Migrate==2.6.0
-pip install "Flask==1.1.4"
-pip install "werkzeug==1.0.1"
-'''
-
 '''flask_migrate命令
-初始化数据库：python main.py db init
-迁移新更改：python main.py db migrate
-升级：python main.py db upgrade
-查看當前版本：python main.py db current
+初始化数据库：flask db init
+迁移新更改：flask db migrate
+升级：flask db upgrade
+查看當前版本：flask db current
 '''
 
 if __name__=="__main__":
@@ -28,8 +16,6 @@ if __name__=="__main__":
     $ export FLASK_ENV=production
     $ export FLASK_APP=main.py
     $ flask run --reload --debugger --host 0.0.0.0 --port 80
-    OR
-    $ python main.py runserver --host 0.0.0.0 --port 80
     '''
     #Development Environment:
-    manager.run()
+    app.run()
