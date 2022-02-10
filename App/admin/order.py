a99610b55aa24bdd982cdad52fb71117460b9653
Mia Song <mia721015@gmail.com>
2022-02-11 01:25:58 +0800

    cancel order from admin_order.html

--- App/admin/order.py
+++ App/admin/order.py
@@ -1,8 +1,9 @@
-from flask import render_template, session, request
+from flask import jsonify, render_template, session, request, abort
 
 from . import admin
 from .auth import login_required
-from App.models import Order
+from App.models import Order, Mydb
+from App import db
 
 @admin.route("/order")
 @login_required
@@ -13,9 +14,8 @@ def get_orders():
     try:
         page = request.args.get("page", 1)
         end_index = limit*int(page)
-
     except ValueError:
-        return render_template("404.html"), 404
+        abort(404)
 
     if request.args.get("status"):
         orders = Order.query.filter_by(
@@ -31,11 +31,11 @@ def get_orders():
         ).order_by(
             Order.create_datetime.desc()
         ).all()
-    
+
     return render_template(
         "admin_order.html", 
         current_user=session.get("user")[1],
-        orders = orders[end_index-limit:end_index+1]
+        orders = orders[end_index-limit:end_index]
     )
 
 @admin.route("/order/<oid>", methods=["GET", "PUT", "DELETE"])
@@ -47,15 +47,36 @@ def get_order_by_id(oid):
     print(order.booked)
     print(order.payment)
     
-    # 修改訂單狀態為：PAID，及付款資料
+    # 修改訂單狀態為：PAID，及修改付款資料
     if request.method == "PUT":
         update_data = request.get_json()
         update_user = session.get("user")[0]
+
+        if order.payment and order.status.value=="PENDING":
+            pass
+        elif order.payment is None and order.status.value=="NEW":
+            pass
+        
         print(order)
 
     # 修改訂單狀態為：CANCEL
     elif request.method == "DELETE":
-        pass
+        if order.status.value=="NEW" or order.status.value=="PENDING":
+            try:
+                order.status = "CANCEL"
+                order.update_user = session.get("user")[0]
+                mydb = Mydb()
+                mydb.cancelBooking()
+            
+            except Exception as e:
+                print("資料庫錯誤：", e)
+            
+            finally:
+                db.session.commit()
+                del mydb
+                
+        else:
+            return jsonify({"error": True, "message": f"Can Not Cancel Order:{oid}"}), 403
     
     return render_template(
         "admin_order.html", 
